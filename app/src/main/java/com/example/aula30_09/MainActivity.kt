9700ccab74092898302197aca2983352777b0ab0
juliosn <julio.neves01@fatec.sp.gov.br>
2024-10-04 20:06:01 -0300

    project_files

---
+++ app/src/main/java/com/example/aula30_09/MainActivity.kt
@@ -0,0 +1,141 @@
+package com.example.aula30_09
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.aula30_09.ui.theme.Aula30_09Theme
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.firestore
+
+class MainActivity : ComponentActivity() {
+    val db = Firebase.firestore
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Aula30_09Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    App(db)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun App(db: FirebaseFirestore){
+    var nome by remember {
+        mutableStateOf("")
+    }
+    var telefone by remember {
+        mutableStateOf("")
+    }
+    Column(
+        Modifier
+            .fillMaxWidth()
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+        }
+        Row(
+            Modifier
+                .fillMaxWidth(),
+            Arrangement.Center
+        ) {
+            Text(text = "App Firebase Firestore")
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                Modifier
+                    .fillMaxWidth(0.3f)
+            ) {
+                Text(text = "Nome:")
+            }
+            Column(
+            ) {
+                TextField(
+                    value = nome,
+                    onValueChange = { nome = it  }
+                )
+            }
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+        ) {
+            Column(
+                Modifier
+                    .fillMaxWidth(0.3f)
+            ) {
+                Text(text = "Telefone:")
+            }
+            Column(
+            ) {
+                TextField(
+                    value = telefone,
+                    onValueChange = { telefone = it  }
+                )
+            }
+        }
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+        }
+        Row(
+            Modifier
+                .fillMaxWidth(),
+            Arrangement.Center
+        ){
+            Button(onClick = {
+                val city = hashMapOf(
+                    "nome" to nome,
+                    "telefone" to telefone
+                )
+                db.collection("clientes").document("PrimeiroCliente")
+                    .set(city)
+                    .addOnSuccessListener { Log.d(ContentValues.TAG, "DocumentSnapshot successfully written!") }
+                    .addOnFailureListener { e -> Log.w(ContentValues.TAG, "Error writing document", e) }
+            }) {
+                Text(text = "Cadastrar")
+            }
+        }
+    }
+}
